6d60d08d74e53d1b42b2ebc41b61b56e9ed96782
노민솔 <nominsol@nominsol-ui-MacBookPro.local>
2026-06-18 10:49:29 +0900

    api 수정

--- js/modifyPassword.js
+++ js/modifyPassword.js
@@ -100,7 +100,7 @@ const modifyPassword = async () => {
 
     if (status == HTTP_CREATED) {
         try {
-            await fetch(`${getServerUrl()}/v1/auth/logout`, {
+            await fetch(`${getServerUrl()}/api/auth/logout`, {
                 method: 'POST',
                 credentials: 'include',
             });
