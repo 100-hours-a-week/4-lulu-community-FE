cf43d3848e40c32f78b7f9019ac3939d5e3c95f6
노민솔 <nominsol@nominsol-ui-MacBookPro.local>
2026-06-18 15:29:42 +0900

    import 추가

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import BoardItem from '../component/board/boardItem.js';
 import Dialog from '../component/dialog/dialog.js';
 import Header from '../component/header/header.js';
-import { authCheck, getServerUrl, prependChild, resolveImageUrl } from '../utils/function.js';
+import { authCheck, getServerUrl, prependChild, resolveImageUrl, getAuthHeader } from '../utils/function.js';
 import { getPosts, searchPosts } from '../api/indexRequest.js';
 
 const DEFAULT_PROFILE_IMAGE = '../public/image/profile/default.jpg';
